8191a985a82632358e2e4aa0d546b1b126c59f75
Chris <christian.jacome.mora@udla.edu.ec>
2024-12-18 14:27:03 -0500

    First Commit

---
+++ src/main/java/com/business/employee/expenses/EmployeeExpensesApplication.java
@@ -0,0 +1,13 @@
+package com.business.employee.expenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeExpensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeExpensesApplication.class, args);
+	}
+
+}
