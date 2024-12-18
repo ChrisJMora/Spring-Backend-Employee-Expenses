714dac3179b56defa72d80affa5f9251e4c27ec4
Chris <christian.jacome.mora@udla.edu.ec>
2024-12-18 14:29:12 -0500

    New: HTTP response models

---
+++ src/main/java/com/business/employee/expenses/models/httpResponse/ApiResult.java
@@ -0,0 +1,11 @@
+package com.example.demo.model.httpResponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class ApiResult {
+    private ResponseStatus status;
+}
+
