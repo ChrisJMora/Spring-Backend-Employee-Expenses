5104b06a00453e7bf896aff36985499f92d25c00
Chris <christian.jacome.mora@udla.edu.ec>
2025-01-23 21:51:45 -0500

    New: SNAPSHOT for deploying

--- src/main/java/com/business/employee/expenses/models/humanResources/ExpenseType.java
+++ src/main/java/com/business/employee/expenses/models/humanResources/ExpenseType.java
@@ -3,21 +3,26 @@ package com.business.employee.expenses.models.humanResources;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+import java.nio.charset.StandardCharsets;
+
 @Getter
 @AllArgsConstructor
 public enum ExpenseType {
-    MEALS("Alimentación"),
+    MEALS("Alimentacion"),
     ACCOMMODATION("Alojamiento"),
     SUPPLIES("Suministros");
 
     private final String description;
 
     public static ExpenseType fromDescription(String description) {
+        String fixedDescription = new String(description.getBytes(StandardCharsets.ISO_8859_1), StandardCharsets.UTF_8);
+
         for(ExpenseType type : ExpenseType.values()) {
-            if (type.getDescription().equals(description)) {
+            if (type.getDescription().equals(fixedDescription)) {
                 return type;
             }
         }
         throw new IllegalArgumentException("No enum constant for description: " + description);
     }
+
 }
